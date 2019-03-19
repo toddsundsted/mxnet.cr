6366ca2741a933b9ba86fe16449d5de37511da70
Todd Sundsted <todd@sundsted.com>
2019-03-19 19:06:16 -0400

    Add toggle for examples that require GPU support.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,14 @@
 require "spec"
 require "../src/mxnet"
+
+def gpu_enabled?
+  begin
+    {% if compare_versions(MXNet::Internal::MXNET_VERSION, "1.3.0") >= 0 %}
+      MXNet::Context.num_gpus > 0
+    {% else %}
+      MXNet::NDArray.array([0], ctx: MXNet.gpu(0)) && true
+    {% end %}
+  rescue MXNet::Internal::LibraryException | MXNet::MXNetException
+    false
+  end
+end

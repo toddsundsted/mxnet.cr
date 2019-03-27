2525c0db3635abd94788126282abca52da769558
Todd Sundsted <todd@sundsted.com>
2019-03-27 11:39:40 -0500

    Handle `NDArray` closeness in specs.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -35,6 +35,49 @@ module Spec
       previous_def
     end
   end
+
+  struct CloseExpectation(T, D)
+    def match(actual_value)
+      expected_value = @expected_value
+      if actual_value.is_a?(MXNet::NDArray) && expected_value.is_a?(MXNet::NDArray)
+        return false unless expected_value.dtype == actual_value.dtype
+        return false unless expected_value.shape == actual_value.shape
+        e_raw_t = expected_value.raw
+        a_raw_t = actual_value.raw
+        case a_raw_t
+        when Array(Float32)
+          return false if e_raw_t.zip(a_raw_t).any? { |e, a| (e - a).abs > @delta }
+        when Array(Float64)
+          return false if e_raw_t.zip(a_raw_t).any? { |e, a| (e - a).abs > @delta }
+        when Array(UInt8)
+          return false if e_raw_t.zip(a_raw_t).any? { |e, a| (e - a).abs > @delta }
+        when Array(Int32)
+          return false if e_raw_t.zip(a_raw_t).any? { |e, a| (e - a).abs > @delta }
+        when Array(Int8)
+          return false if e_raw_t.zip(a_raw_t).any? { |e, a| (e - a).abs > @delta }
+        when Array(Int64)
+          return false if e_raw_t.zip(a_raw_t).any? { |e, a| (e - a).abs > @delta }
+        end
+        return true
+      end
+      previous_def
+    end
+
+    def failure_message(actual_value)
+      expected_value = @expected_value
+      if actual_value.is_a?(MXNet::NDArray) && expected_value.is_a?(MXNet::NDArray)
+        expected_value = expected_value.to_s.split("\n")
+        actual_value = actual_value.to_s.split("\n")
+        return <<-MSG
+          Expected: #{expected_value[0]} to be within #{@delta}
+                    #{expected_value[1]}
+                of: #{actual_value[0]}
+                    #{actual_value[1]}
+          MSG
+      end
+      previous_def
+    end
+  end
 end
 
 def gpu_enabled?

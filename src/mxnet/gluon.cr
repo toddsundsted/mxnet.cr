865e19b404273f7a8b56b73ce8abe5aab54120f9
Todd Sundsted <todd@sundsted.com>
2019-04-10 20:31:07 -0400

    `Gluon` module.

---
+++ src/mxnet/gluon.cr
@@ -0,0 +1,12 @@
+require "../mxnet"
+
+module MXNet
+  # The `Gluon` library is a high-level interface for MXNet designed
+  # to be easy to use, while keeping most of the flexibility of a low
+  # level API. `Gluon` supports both imperative and symbolic
+  # programming, making it easy to train complex models imperatively
+  # and then to deploy as a symbolic graph.
+  #
+  module Gluon
+  end
+end

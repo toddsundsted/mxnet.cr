6ad8eb5d4519acfb54f4ffb29e8fa153aa10ebd4
Todd Sundsted <todd@sundsted.com>
2019-04-17 22:00:54 -0400

    Clean up library declarations.

--- src/mxnet/libmxnet.cr
+++ src/mxnet/libmxnet.cr
@@ -41,13 +41,37 @@ module MXNet
         dtype : Int32,
         handle : NDArrayHandle*
       ) : Int32
-      fun MXNDArraySyncCopyFromCPU(handle : NDArrayHandle, data : Void*, size : LibC::SizeT) : Int32
-      fun MXNDArraySyncCopyToCPU(handle : NDArrayHandle, data : Void*, size : LibC::SizeT) : Int32
-      fun MXNDArrayGetGrad(handle : NDArrayHandle, grad_handle : NDArrayHandle*) : Int32
-      fun MXNDArrayGetShape(handle : NDArrayHandle, dim : UInt32*, pdata : UInt32**) : Int32
-      fun MXNDArrayGetContext(handle : NDArrayHandle, dev_type : Int32*, dev_id : Int32*) : Int32
-      fun MXNDArrayGetDType(handle : NDArrayHandle, dtype : UInt32*) : Int32
-      fun MXNDArrayFree(handle : NDArrayHandle) : Int32
+      fun MXNDArraySyncCopyFromCPU(
+        handle : NDArrayHandle,
+        data : Void*,
+        size : LibC::SizeT
+      ) : Int32
+      fun MXNDArraySyncCopyToCPU(
+        handle : NDArrayHandle,
+        data : Void*,
+        size : LibC::SizeT
+      ) : Int32
+      fun MXNDArrayGetGrad(
+        handle : NDArrayHandle,
+        grad_handle : NDArrayHandle*
+      ) : Int32
+      fun MXNDArrayGetShape(
+        handle : NDArrayHandle,
+        dim : UInt32*,
+        pdata : UInt32**
+      ) : Int32
+      fun MXNDArrayGetContext(
+        handle : NDArrayHandle,
+        dev_type : Int32*,
+        dev_id : Int32*
+      ) : Int32
+      fun MXNDArrayGetDType(
+        handle : NDArrayHandle,
+        dtype : UInt32*
+      ) : Int32
+      fun MXNDArrayFree(
+        handle : NDArrayHandle
+      ) : Int32
       fun MXAutogradMarkVariables(
         num_var : MXUInt,
         var_handles : NDArrayHandle*,
@@ -66,22 +90,43 @@ module MXNet
         grad_handles : NDArrayHandle **,
         grad_stypes : Int32**
       ) : Int32
-      fun MXAutogradSetIsRecording(is_recording : Int32, previous : Int32*) : Int32
-      fun MXAutogradSetIsTraining(is_training : Int32, previous : Int32*) : Int32
-      fun MXAutogradIsRecording(current : Bool*) : Int32
-      fun MXAutogradIsTraining(current : Bool*) : Int32
-      fun NNGetOpHandle(name : UInt8*, op : OpHandle*) : Int32
+      fun MXAutogradSetIsRecording(
+        is_recording : Int32,
+        previous : Int32*
+      ) : Int32
+      fun MXAutogradSetIsTraining(
+        is_training : Int32,
+        previous : Int32*
+      ) : Int32
+      fun MXAutogradIsRecording(
+        current : Bool*
+      ) : Int32
+      fun MXAutogradIsTraining(
+        current : Bool*
+      ) : Int32
+      fun NNGetOpHandle(
+        name : UInt8*,
+        op : OpHandle*
+      ) : Int32
       fun MXImperativeInvoke(
         creator : OpHandle,
-        num_inputs : Int32, inputs : NDArrayHandle*,
-        num_outputs : Int32*, outputs : NDArrayHandle**,
-        num_params : Int32, param_keys : UInt8**, param_vals : UInt8**
+        num_inputs : Int32,
+        inputs : NDArrayHandle*,
+        num_outputs : Int32*,
+        outputs : NDArrayHandle**,
+        num_params : Int32,
+        param_keys : UInt8**,
+        param_vals : UInt8**
       ) : Int32
       fun MXSymbolCreateVariable(
         name : UInt8*,
         handle : SymbolHandle*
       ) : Int32
-      fun MXSymbolGetName(handle : SymbolHandle, name : UInt8**, success : Int32*) : Int32
+      fun MXSymbolGetName(
+        handle : SymbolHandle,
+        name : UInt8**,
+        success : Int32*
+      ) : Int32
       fun MXSymbolSetAttr(
         handle : SymbolHandle,
         key : UInt8*,
@@ -103,9 +148,19 @@ module MXNet
         size : MXUInt*,
         out : UInt8***
       ) : Int32
-      fun MXSymbolListArguments(handle : SymbolHandle, size : MXUInt*, str_array : UInt8***) : Int32
-      fun MXSymbolListOutputs(handle : SymbolHandle, size : MXUInt*, str_array : UInt8***) : Int32
-      fun MXSymbolFree(handle : SymbolHandle) : Int32
+      fun MXSymbolListArguments(
+        handle : SymbolHandle,
+        size : MXUInt*,
+        str_array : UInt8***
+      ) : Int32
+      fun MXSymbolListOutputs(
+        handle : SymbolHandle,
+        size : MXUInt*,
+        str_array : UInt8***
+      ) : Int32
+      fun MXSymbolFree(
+        handle : SymbolHandle
+      ) : Int32
       fun MXExecutorBindEX(
         handle : SymbolHandle,
         dev_type : Int32, dev_id : Int32,
@@ -137,7 +192,9 @@ module MXNet
         num_outputs : UInt32*,
         outputs : NDArrayHandle**
       ) : Int32
-      fun MXExecutorFree(handle : ExecutorHandle) : Int32
+      fun MXExecutorFree(
+        handle : ExecutorHandle
+      ) : Int32
       fun MXSymbolCreateAtomicSymbol(
         creator : OpHandle,
         num_param : MXUInt,

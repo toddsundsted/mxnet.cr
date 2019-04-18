7e284c74939d519107bf2502b2c482e517fc86da
Todd Sundsted <todd@sundsted.com>
2019-04-17 21:29:40 -0400

    Fill out `Block` class.

--- spec/src/mxnet/gluon/block_spec.cr
+++ spec/src/mxnet/gluon/block_spec.cr
@@ -1,7 +1,197 @@
 require "../../../spec_helper"
 require "../../../../src/mxnet/gluon"
 
+describe MXNet::Gluon::BlockScope do
+  describe ".create" do
+    it "creates prefix with a counter" do
+      prefix, _ = MXNet::Gluon::BlockScope.create(nil, nil, "foo")
+      prefix.should match(/^foo[0-9]+_$/)
+    end
+
+    it "creates a ParameterDict with a prefix" do
+      _, params = MXNet::Gluon::BlockScope.create(nil, nil, "foo")
+      params.prefix.should match(/^foo[0-9]+_$/)
+    end
+  end
+end
+
+create_double(MXNet::Gluon::Block)
+
+class FooBar < MXNet::Gluon::Block
+  attribute \
+    foo : MXNet::Gluon::Parameter | MXNet::Gluon::Block,
+    bar : MXNet::Gluon::Parameter | MXNet::Gluon::Block
+end
+
 describe MXNet::Gluon::Block do
+  context "attribute access" do
+    block = MXNet::Gluon::Block.new
+
+    it "sets a block attribute" do
+      a = block.set_attr("a", MXNet::Gluon::Block.new)
+      block.get_attr("a").should be(a)
+    end
+
+    it "sets a parameter attribute" do
+      b = block.set_attr("b", MXNet::Gluon::Parameter.new("b"))
+      block.get_attr("b").should be(b)
+    end
+
+    it "raises exception if attribute is set to another type" do
+      expect_raises(Exception) do
+        block.set_attr("c", MXNet::Gluon::Block.new)
+        block.set_attr("c", MXNet::Gluon::Parameter.new("c"))
+      end
+    end
+
+    it "raises exception if attribute is not set" do
+      expect_raises(Exception) do
+        block.get_attr("d")
+      end
+    end
+  end
+
+  context "attribute assignment" do
+    block = FooBar.new
+
+    it "assigns a block attribute" do
+      foo = block.foo = MXNet::Gluon::Block.new
+      block.foo.should be(foo)
+    end
+
+    it "assigns a parameter attribute" do
+      bar = block.bar = MXNet::Gluon::Parameter.new("bar")
+      block.bar.should be(bar)
+    end
+  end
+
+  describe "#new" do
+    context "au naturale" do
+      block = MXNet::Gluon::Block.new
+
+      it "assigns a unique prefix" do
+        block.prefix.should_not eq(MXNet::Gluon::Block.new.prefix)
+        block.prefix.should match(/^block[0-9]+_$/)
+      end
+    end
+
+    context "with prefix" do
+      block = MXNet::Gluon::Block.new(prefix: "foo")
+
+      it "uses the assigned prefix" do
+        block.prefix.should eq("foo")
+      end
+    end
+
+    context "with params" do
+      params = MXNet::Gluon::ParameterDict.new.tap do |params|
+        params.get("foo")
+      end
+
+      block = MXNet::Gluon::Block.new(params: params)
+
+      it "shares the assigned params" do
+        block.params.get("foo").should be(params.get("foo"))
+      end
+    end
+  end
+
+  describe "#with_name_scope" do
+    block = FooBar.new
+
+    it "prepends prefixes to scoped blocks" do
+      block.with_name_scope do
+        block.foo = FooBar.new
+        block.foo.as(FooBar).with_name_scope do
+          block.foo.as(FooBar).bar = FooBar.new
+        end
+      end
+      block.foo.as(FooBar).prefix.should match(/^foobar[0-9]+_foobar0_$/)
+      block.foo.as(FooBar).bar.as(FooBar).prefix.should match(/^foobar[0-9]+_foobar0_foobar0_$/)
+    end
+  end
+
+  describe "#collect_params" do
+    block =
+      MXNet::Gluon::Block.new(prefix: "block_").tap do |block|
+        block.params.get("foo")
+        block.params.get("bar")
+        block.params.get("baz")
+      end
+    child =
+      MXNet::Gluon::Block.new(prefix: "block_").tap do |block|
+        block.params.get("qoz")
+      end
+
+    it "returns all its parameters" do
+      params = MXNet::Gluon::ParameterDict.new(prefix: "block_").tap do |params|
+        params.get("foo")
+        params.get("bar")
+        params.get("baz")
+      end
+      block.collect_params.should eq(params)
+    end
+
+    it "returns the matching parameters" do
+      params = MXNet::Gluon::ParameterDict.new(prefix: "block_").tap do |params|
+        params.get("bar")
+        params.get("baz")
+      end
+      block.collect_params(/_ba/).should eq(params)
+    end
+
+    it "returns matching parameters from children" do
+      block.set_attr("qoz", child)
+      params = MXNet::Gluon::ParameterDict.new(prefix: "block_").tap do |params|
+        params.get("qoz")
+      end
+      block.collect_params(/_q/).should eq(params)
+    end
+  end
+
+  describe "#init" do
+    block =
+      MXNet::Gluon::Block.new.tap do |block|
+        block.params.get("foo", shape: 1)
+      end
+
+    it "initializes all parameters" do
+      block.init
+      block.params.get("foo").data.should be_a(MXNet::NDArray)
+    end
+  end
+
+  describe "#register_child" do
+    block = MXNet::Gluon::Block.new
+    child = MXNet::Gluon::Block.new
+
+    it "registers block" do
+      block.register_child(child)
+      block.get_attr("0").should eq(child)
+    end
+  end
+
+  describe "#register_parameter" do
+    block = MXNet::Gluon::Block.new
+    param = MXNet::Gluon::Parameter.new("param")
+
+    it "registers parameter" do
+      block.register_parameter(param)
+      block.get_attr("param").should eq(param)
+    end
+  end
+
+  describe "#hybridize" do
+    block = MXNet::Gluon::Block.new
+    child = instance_double(MXNet::Gluon::Block)
+
+    it "calls hybridize on its children" do
+      block.register_child(child)
+      block.hybridize
+      verify(child, hybridize: once)
+    end
+  end
+
   describe "#forward" do
     context "given a Symbol" do
       it "must be implemented in a subclass" do
@@ -22,6 +212,17 @@ describe MXNet::Gluon::Block do
 end
 
 describe MXNet::Gluon::HybridBlock do
+  describe "#register_child" do
+    block = MXNet::Gluon::HybridBlock.new
+    child = MXNet::Gluon::Block.new
+
+    it "doesn't accept a block" do
+      expect_raises(Exception) do
+        block.register_child(child)
+      end
+    end
+  end
+
   describe "#hybrid_forward" do
     context "given a Symbol" do
       it "must be implemented in a subclass" do
--- src/mxnet/gluon/block.cr
+++ src/mxnet/gluon/block.cr
@@ -2,10 +2,203 @@ require "../gluon"
 
 module MXNet
   module Gluon
+    # Scope for collecting child `Blocks`.
+    #
+    class BlockScope
+      protected def initialize(@block : Block? = nil)
+        @counters = Hash(String, Int32).new { -1 }
+      end
+
+      getter :block
+      getter :counters
+
+      @@current : self? = nil
+
+      # Creates prefix and params for new `Block`.
+      #
+      def self.create(prefix, params, hint) : {String, ParameterDict}
+        current = @@current ||= BlockScope.new
+        if (block = current.block)
+          if prefix.nil?
+            prefix = "#{hint}#{current.counters[hint] += 1}_"
+          end
+          if params.nil?
+            params = ParameterDict.new(prefix: "#{block.prefix}#{prefix}")
+          else
+            params = ParameterDict.new(prefix: "#{block.prefix}#{prefix}", shared: params)
+          end
+          {"#{block.prefix}#{prefix}", params}
+        else
+          if prefix.nil?
+            prefix = "#{hint}#{current.counters[hint] += 1}_"
+          end
+          if params.nil?
+            params = ParameterDict.new(prefix: prefix)
+          else
+            params = ParameterDict.new(prefix: params.prefix, shared: params)
+          end
+          {prefix, params}
+        end
+      end
+
+      def call(block)
+        previous, @@current = @@current, block.scope
+        yield
+      ensure
+        @@current = previous
+      end
+    end
+
     # Base class for all neural network layers and models. Your models
     # should subclass this class.
     #
     class Block
+      # Creates accessors for declared attributes.
+      #
+      macro attribute(*names)
+        {% for name in names %}
+          {% if name.is_a?(TypeDeclaration) %}
+            def {{name.var.id}}
+              get_attr("{{name.var.id}}").as({{name.type}})
+            end
+            def {{name.var.id}}? : {{name.type}} | Nil
+              (value = get_attr("{{name.var.id}}")) ? value.as({{name.type}}) : nil
+            end
+            def {{name.var.id}}=({{name.var.id}} : {{name.type}} | Nil)
+              set_attr("{{name.var.id}}", {{name.var.id}})
+            end
+          {% elsif name.is_a?(Call) %}
+            {% raise "must include a type declaration: #{name}" %}
+          {% else %}
+            {% raise "must be a type declaration: #{name}" %}
+          {% end %}
+        {% end %}
+      end
+
+      @reg_children = Hash(String, MXNet::Gluon::Block).new
+      @reg_parameters = Hash(String, MXNet::Gluon::Parameter).new
+      @reg_other = Hash(String, Nil).new
+
+      def initialize(prefix = nil, params = nil)
+        @prefix, @params = BlockScope.create(prefix, params, hint)
+        @scope = BlockScope.new(self)
+      end
+
+      # Scope of this block.
+      getter scope
+
+      # Prefix of this block.
+      getter prefix
+
+      # Returns this block's parameter dictionary.
+      #
+      # Does not include its children's parameters.
+      #
+      getter params
+
+      # Enters a name scope managing block names.
+      #
+      #     self.with_name_scope do
+      #       self.dense = MXNet::Gluon::NN.Dense(20)
+      #     end
+      #
+      def with_name_scope
+        unless scope = @scope
+          raise Exception.new(
+            "Ensure that parent classes are initialized by calling " \
+            "`super(...)` in #{self.class}#initialize()."
+          )
+        end
+        scope.call(self) do
+          yield
+        end
+      end
+
+      # Returns this block's registered children.
+      #
+      def children
+        @reg_children.values
+      end
+
+      # Returns a `ParameterDict` containing this `Block`'s and all of
+      # its children's `Parameter`s. Can also return the `Parameter`s
+      # that match some given regular expressions.
+      #
+      # For example, collect the specified `Parameter`s for
+      # "conv1_weight", "conv1_bias", "fc_weight" and "fc_bias":
+      #
+      #     model.collect_params(/conv1_weight|conv1_bias|fc_weight|fc_bias/)
+      #
+      # or, alternatively, collect all parameters whose names end with
+      # "weight" or "bias":
+      #
+      #     model.collect_params(/.*weight|.*bias/)
+      #
+      # ### Parameters
+      # * *selector* (`Regex`)
+      #   Regular expressions to match parameters.
+      #
+      def collect_params(selector = nil)
+        ret = ParameterDict.new(prefix: @params.prefix)
+        if selector
+          ret.update(@params.select { |(k, _)| k =~ selector })
+        else
+          ret.update(@params)
+        end
+        @reg_children.each_value do |child|
+          ret.update(child.collect_params(selector))
+        end
+        ret
+      end
+
+      # Initializes parameters of this block and its children.
+      # Equivalent to `self.collect_params.init(...)`.
+      #
+      # ### Parameters
+      # * *init* (`Initializer`, default = `nil`)
+      #   The initializer to use.
+      # * *ctx* (`Context` or `Array(Context)`, default = `nil`)
+      #   Desired contexts.
+      # * *force_reinit* (`Bool`, default = `false`)
+      #   Whether to force re-initialization if parameter is already
+      #   initialized.
+      #
+      def init(init = nil, ctx = nil, force_reinit = false)
+        collect_params.init(init: init, ctx: ctx, force_reinit: force_reinit)
+        self
+      end
+
+      # Registers block as a child of self. Blocks assigned as
+      # attributes will be registered automatically.
+      #
+      def register_child(block, name = nil)
+        name = @reg_children.size.to_s unless name
+        @reg_children[name] = block
+        block
+      end
+
+      # Registers parameter on self. Parameters assigned as attributes
+      # will be registered automatically.
+      #
+      def register_parameter(param, name = nil)
+        name = param.name unless name
+        @reg_parameters[name] = param
+        param
+      end
+
+      # Activates or deactivates `HybridBlock` children
+      # recursively. Has no effect on non-hybrid blocks.
+      #
+      # ### Parameters
+      # * *active* (`Bool`, default = `true`)
+      #   Whether to turn hybridization on or off.
+      #
+      def hybridize(active = true)
+        @reg_children.each_value do |child|
+          child.hybridize(active)
+        end
+      end
+
       # Calls `#forward`.
       #
       # Only accepts positional arguments.
@@ -31,12 +224,54 @@ module MXNet
           "#forward must be implemented in a subclass"
         )
       end
+
+      def set_attr(name : String, value : Block | Parameter | Nil)
+        [@reg_children, @reg_parameters, @reg_other].each do |reg|
+          if reg.has_key?(name) && reg[name].class != value.class
+            raise Exception.new(
+              "changing attribute class for #{name} " \
+              "from #{reg[name].class} to #{value.class} " \
+              "is not allowed"
+            )
+          end
+        end
+        case value
+        when Block
+          register_child(value, name)
+        when Parameter
+          register_parameter(value, name)
+        else
+          @reg_other[name] = value
+        end
+      end
+
+      def get_attr(name : String) : Block | Parameter | Nil
+        [@reg_children, @reg_parameters, @reg_other].each do |reg|
+          return reg[name] if reg.has_key?(name)
+        end
+        raise Exception.new("undefined attribute #{name}")
+      end
+
+      private def hint
+        self.class.name.split("::").last.downcase
+      end
     end
 
     # `HybridBlock` supports forwarding with both `Symbol` and
     # `NDArray`.
     #
     class HybridBlock < Block
+      def register_child(block, name = nil)
+        unless block.is_a?(MXNet::Gluon::HybridBlock)
+          raise Exception.new(
+            "Children of HybridBlock must also be HybridBlocks, " \
+            "but #{block} has type #{block.class}. If you are using " \
+            "Sequential, please try HybridSequential instead."
+          )
+        end
+        super
+      end
+
       # Defines the forward computation.
       #
       # ### Parameters

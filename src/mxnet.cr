7c805cc7d185a28f348073beb49cb653aa7753ba
Todd Sundsted <todd@sundsted.com>
2020-01-20 19:52:11 -0500

    Update version.

--- src/mxnet.cr
+++ src/mxnet.cr
@@ -17,7 +17,7 @@
 # optimizations on the symbolic graph.
 #
 module MXNet
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 
   {% begin %}
     {% version = `python '#{__DIR__}/mxnet/libmxnet.py' version`.stringify.chomp %}

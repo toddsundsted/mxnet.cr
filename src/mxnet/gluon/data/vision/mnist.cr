c9d9b403e2949977030336a0fc9e62d747e4c0f3
Todd Sundsted <todd@sundsted.com>
2020-02-23 11:27:01 -0500

    Refactor `MNIST` dataset.
    Introduce a type variable to parameterize the underlying `Indexable`
    generic (rather than hardcode it to `Tuple(MXNet::NDArray, Int32)`).
    This allows the `transform` proc more flexibility when transforming
    the samples.

--- src/mxnet/gluon/data/vision/mnist.cr
+++ src/mxnet/gluon/data/vision/mnist.cr
@@ -5,13 +5,39 @@ module MXNet
   module Gluon
     module Data
       module Vision
-        class MNIST < MXNet::Gluon::Data::DownloadedDataset(MXNet::NDArray, Int32, Tuple(MXNet::NDArray, Int32))
+        class MNIST(T) < MXNet::Gluon::Data::DownloadedDataset(MXNet::NDArray, Int32, T)
           TRAIN_DATA = {"train-images-idx3-ubyte.gz", "6c95f4b05d2bf285e1bfb0e7960c31bd3b3f8a7d"}
           TRAIN_LABEL = {"train-labels-idx1-ubyte.gz", "2a80914081dc54586dbdf242f9805a6b8d2a15fc"}
           TEST_DATA = {"t10k-images-idx3-ubyte.gz", "c3a25af1f52dad7f726cce8cacb138654b760d48"}
           TEST_LABEL = {"t10k-labels-idx1-ubyte.gz", "763e7fa3757d93b0cdec073cef058b2004252c17"}
 
-          def initialize(root = File.join("~/", ".mxnet", "datasets", "mnist"), @train = true, transform = nil)
+          # Creates a new instance.
+          #
+          # ### Parameters
+          # * *root* (`String`, optional)
+          #   Directory in which to cache downloaded files.
+          #   Automatically created if it does not already exist.
+          # * *train* (`Bool`, optional)
+          #   Whether to load the training or testing data.
+          #
+          def self.new(root = File.join("~/", ".mxnet", "datasets", "mnist"), train = true)
+            MNIST(Tuple(MXNet::NDArray, Int32)).new(transform: nil, root: root, train: train)
+          end
+
+          # Creates a new instance.
+          #
+          # Transforms each sample with the supplied transformer.
+          #
+          # ### Parameters
+          # * *transform* (`Proc`, required)
+          #   Transformation to apply to each sample.
+          # * *root* (`String`, optional)
+          #   Directory in which to cache downloaded files.
+          #   Automatically created if it does not already exist.
+          # * *train* (`Bool`, optional)
+          #   Whether to load the training or testing data.
+          #
+          def initialize(transform : Proc(MXNet::NDArray, Int32, T)?, root = File.join("~/", ".mxnet", "datasets", "mnist"), @train = true)
             super(root: root, transform: transform)
           end
 
